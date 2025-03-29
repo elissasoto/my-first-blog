12b95be31fb18bb3f85158ffee88f4bc7718d5fb
Elissa Soto <elissasoto@outlook.com>
2025-03-29 13:44:24 -0400

    My Django Girls app, first commit

---
+++ pythonintro.py
@@ -0,0 +1,9 @@
+
+def hi(name):
+    if name == 'Elissa':
+        print ('Hi Elissa')
+    elif name == 'Sonja':
+        print ('Hi Sonja!')
+    else:
+        print ('Hi anonymous!')
+hi('Elissa')
